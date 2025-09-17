62a82189be339c2e5c8f17338b1fff21297185f9
Lavanya3346 <lavanyaeede2883@gmail.com>
2025-09-17 15:13:54 +0530

    Program2.java

---
+++ Program2.java
@@ -0,0 +1,11 @@
+public class Problem2 {
+		    public static void main(String[] args) {
+		        int a = 9;
+		        for (int i = 1; i <= a; i++) {
+		            System.out.print((2 * i - 1));
+		            if (i < a) {
+		                System.out.print(", ");
+		            }
+		        }
+		    }
+		}
